ed328cafec6cdd53e5fcae2266c0022b71a62152
moqiaoduo <415372363@qq.com>
2020-08-19 14:48:39 +0800

    若页面顺序非数字，则自动分配

--- var/App/Admin/Page/Save.php
+++ var/App/Admin/Page/Save.php
@@ -79,7 +79,7 @@ class Save extends NoRender
         }
 
         if ($p['type'] == 'page') // 只有发布的时候才能更改页面顺序
-            $page->order = $p['order'] ?: $this->getNewOrderNum();
+            $page->order = is_numeric($p['order']) ? $p['order'] : $this->getNewOrderNum(); // 非数字自动分配
 
         $page->template = $p['template'];
         $page->status = $p['visibility'];
